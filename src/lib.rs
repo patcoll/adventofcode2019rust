d888333f361c0672b9bea3a577ba2e632bcd1013
Patrick Collins <pat@burned.com>
2019-12-13 18:18:41 -0500

    Add failing test for day 3, part 2

--- src/lib.rs
+++ src/lib.rs
@@ -111,4 +111,21 @@ mod test {
         assert_eq!(c, Coordinate { x: -369, y: 6 });
         assert_eq!(c.manhattan_distance(), 375);
     }
+
+    #[test]
+    fn test_day_03_part_2() {
+        let test_grid_1 = Grid::from(&vec![
+            Route::from("R75,D30,R83,U83,L12,D49,R71,U7,L72"),
+            Route::from("U62,R66,U55,R34,D71,R55,D58,R83"),
+        ]);
+
+        assert_eq!(test_grid_1.intersection_shortest_path(), 610);
+
+        let test_grid_2 = Grid::from(&vec![
+            Route::from("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"),
+            Route::from("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"),
+        ]);
+
+        assert_eq!(test_grid_2.intersection_shortest_path(), 410);
+    }
 }
